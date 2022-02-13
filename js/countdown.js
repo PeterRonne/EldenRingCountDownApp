2dcb4421c2b9396025790045065562cb4e5ba674
Peter <peter.roest07@gmail.com>
2022-02-13 21:24:19 +0100

    my commit

---
+++ js/countdown.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const eldenRingRelease = new Date('Feb 25 2022')
+
+const eldenRingCountDown = function () {
+    let currentDate = new Date()
+
+    let currentTime = currentDate.getTime()
+    let relesaseTime = eldenRingRelease.getTime()
+
+    let remainingTime = relesaseTime - currentTime
+
+    let remainingSeconds = Math.floor(remainingTime / 1000)
+    let remainingMinutes = Math.floor(remainingSeconds / 60)
+    let remainingHours = Math.floor(remainingMinutes / 60)
+    let remainingDays = Math.floor(remainingHours / 24)
+
+    remainingHours %= 24
+    remainingMinutes %= 60
+    remainingSeconds %= 60
+
+    document.getElementById("Seconds").textContent = remainingSeconds
+    document.getElementById("Hours").textContent = remainingHours
+    document.getElementById("Days").textContent = remainingDays
+    document.getElementById("Minutes").textContent = remainingMinutes
+
+    setTimeout(eldenRingCountDown, 1000)
+}
+
+console.log(eldenRingRelease)
+
+window.onload = eldenRingCountDown()
